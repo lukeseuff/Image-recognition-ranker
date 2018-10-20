408097b82d16184f4d81aaeae76f8ed4378f9cf4
Luke Seuffert <lukeseuff@gmail.com>
2018-10-20 16:14:00 -0400

    loop image predictions
    TagUrls now loops through images and loops through each image's predictions.

--- src/client/tag.go
+++ src/client/tag.go
@@ -25,16 +25,32 @@ type GeneralRequest struct {
 	Inputs []Inputs `json:"inputs"`
 }
 
-func (c Client) TagUrl(url string) error {
+type Tag struct {
+	// TODO: fill out
+}
+
+type TaggedImage struct {
+	// TODO: fill out
+}
+
+const predictUrl string = "https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/versions/aa7f35c01e0642fda5cf400f543e7c40/outputs"
+
+func (c Client) TagUrls(urls []string) error /* map[string]interface{} */ {
 	var reqBody GeneralRequest
-	reqBody.Inputs = append(reqBody.Inputs, Inputs{ ImageData{ Image{ URL: url } } })
+	urlCount := len(urls)
+	inputs := make([]Inputs, urlCount)
+	
+	for i, url := range urls {
+		inputs[i] = Inputs{ ImageData{ Image{ URL: url } } }
+	}
+	
+	reqBody.Inputs = inputs
 	jsonBody, err := json.Marshal(reqBody)
 
 	if err != nil {
 		return err
 	}
-
-	predictUrl := "https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/versions/aa7f35c01e0642fda5cf400f543e7c40/outputs"
+	
 	req, err := http.NewRequest("POST", predictUrl, bytes.NewReader(jsonBody))
 
 	if err != nil {
@@ -57,19 +73,21 @@ func (c Client) TagUrl(url string) error {
 		return err
 	}
 
-	jsonResponse := new(map[string]interface{})
-	err = json.Unmarshal(resBody, jsonResponse)
+	var jsonResponse map[string]interface{}
+	err = json.Unmarshal(resBody, &jsonResponse)
 
 	if err != nil {
 		return err
 	}
 
-	// TODO: remove
-	fmt.Printf("%+v\n", jsonResponse)
-	
+	images := jsonResponse["outputs"].([]interface{})
+	for _, image := range images {
+		predictions := image.(map[string]interface{})["data"].(map[string]interface{})["concepts"].([]interface{})
+		for _, prediction := range predictions {
+			p := prediction.(map[string]interface{})
+			fmt.Println(p["id"], p["name"])
+		}
+	}
+
 	return nil
 }
-
-// func (Client) TagUrls([]string urls) map[string]interface{} {
-	
-// }
--- src/main.go
+++ src/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"client"
-	// "bufio"
+	"bufio"
 	// "bytes"
 	// "encoding/json"
 	"fmt"
@@ -12,12 +12,38 @@ import (
 	// "time"
 )
 
+func getUrls() []string {
+	var urls []string
+	file, err := os.Open("data/images.txt")
+	
+	if err != nil {
+		return urls
+	}
+	
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	
+	for scanner.Scan() {
+		line := scanner.Text()
+		urls = append(urls, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+
+	return urls
+}
+
 func main() {
 	apiClient, err := client.NewClient()
+	
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	
-	apiClient.TagUrl("https://c3.staticflickr.com/6/5615/15629176482_0384ab8a9f_o.jpg")
+	// apiClient.TagUrl("https://c3.staticflickr.com/6/5615/15629176482_0384ab8a9f_o.jpg")
+
+	apiClient.TagUrls(getUrls()[:2])
 }
